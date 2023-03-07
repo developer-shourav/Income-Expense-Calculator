9f275cc22da67a5fce1006692ea9cb5e2eefc2fe
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-07 21:03:43 +0600

    all bug solved and project completed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const calculateExpense = () => {
-  const income = document.querySelector("income").value;
-  const food = document.querySelector("food").value;
-  const rent = document.querySelector("rent").value;
-  const clothes = document.querySelectorAll("clothes").value;
+  const income = document.querySelector("#income").value;
+  const food = document.querySelector("#food").value;
+  const rent = document.querySelector("#rent").value;
+  const clothes = document.querySelector("#clothes").value;
 
   if (
     income < 0 ||
@@ -18,18 +18,16 @@ const calculateExpense = () => {
     return;
   }
   // calculate expense
-  const expense =
-    parseInt(food) + parseInt(rent) + parseInt(clothes);
-
+  const expense = parseFloat(food) + parseFloat(rent) + parseFloat(clothes);
   // calculate balance
-  const balance = parseInt(income.value) - expense;
+  const balance = parseFloat(income) - expense;
   //   validate income
   if (expense > income.value) {
     alert("Expenses cannot be more than income");
   } else {
     // view total expense and balance
-    document.getElementById("total-expense").innerText = expense;
-    document.getElementById("balance").innerText = balance;
+    document.getElementById("total-expense").innerText = expense.toFixed(2);
+    document.getElementById("balance").innerText = balance.toFixed(2);
   }
 };
 
@@ -40,18 +38,20 @@ const calculateSavings = () => {
   if (savePercentage < 0) {
     alert("Provide positive saving value");
   }
-  const savingAmount = (savePercentage / 100) + income;
+
+  const income = document.querySelector("#income").value;
+  const savingAmount = (savePercentage / 100) * parseFloat(income);
 
   // calculate remaining balance
   const balance = document.getElementById("balance").innerText;
-  const remainingBalance = balance - savingAmount;
+  const remainingBalance = parseFloat(balance) - savingAmount;
 
   //   validate saving amount
   if (savingAmount > balance) {
     alert("SavingAmount is greater than balance");
   } else {
     // view saving amount and remaining balance
-    document.getElementById("saving-amount").innerText = savingAmount;
-    document.getElementById("remaining-balance").innerText = remainingBalance;
+    document.getElementById("saving-amount").innerText = savingAmount.toFixed(2);
+    document.getElementById("remaining-balance").innerText = remainingBalance.toFixed(2);
   }
 };
